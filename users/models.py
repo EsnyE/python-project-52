5d854de9df8f8d44403e8cb65e901c0b36db0411
София <sofia@MacBook-Air-Sofia.local>
2026-05-01 15:48:49 +0300

    add .

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=150, verbose_name='Имя')
-    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    first_name = models.CharField(max_length=150, blank=True, verbose_name='Имя')
+    last_name = models.CharField(max_length=150, blank=True, verbose_name='Фамилия')
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
