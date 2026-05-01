8461e2535114ef9c52c19712d035f28a9f3787b8
София <sofia@MacBook-Air-Sofia.local>
2026-05-01 20:13:06 +0300

    add .

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('', views.UsersListView.as_view(), name='users'),
-    path('create/', views.UserCreateView.as_view(), name='user_create'),
-    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='user_update'),
-    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='user_delete'),
+    path('create/', views.UserCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
 ]
