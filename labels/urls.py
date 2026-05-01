b51b73ec4a841ddf802491cb3bd8c6cdee0ff81a
София <sofia@MacBook-Air-Sofia.local>
2026-05-01 20:03:18 +0300

    add .

--- labels/urls.py
+++ labels/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from labels import views
 
 
+app_name = 'labels'
+
 urlpatterns = [
     path('', views.LabelsListView.as_view(), name='labels'),
     path('create/', views.LabelCreateView.as_view(), name='label_create'),
