c9cc3dbef6e0ecc6c2971505479731c25242e5af
София <sofia@MacBook-Air-Sofia.local>
2026-05-01 21:27:11 +0300

    add .

--- labels/urls.py
+++ labels/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from labels import views
 
 urlpatterns = [
-    path('', views.LabelsListView.as_view(), name='list'),
+    path('', views.LabelsListView.as_view(), name='labels'),
     path('create/', views.LabelCreateView.as_view(), name='label_create'),
     path('<int:pk>/update/', views.LabelUpdateView.as_view(), name='label_update'),
     path('<int:pk>/delete/', views.LabelDeleteView.as_view(), name='label_delete'),
--- statuses/urls.py
+++ statuses/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from statuses import views
 
 
-app_name = 'statuses'
-
 urlpatterns = [
-    path('', views.StatusesListView.as_view(), name='list'),
+    path('', views.StatusesListView.as_view(), name='statuses'),
     path('create/', views.StatusCreateView.as_view(), name='status_create'),
     path('<int:pk>/update/', views.StatusUpdateView.as_view(), name='status_update'),
     path('<int:pk>/delete/', views.StatusDeleteView.as_view(), name='status_delete'),
